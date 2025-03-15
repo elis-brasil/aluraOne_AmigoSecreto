56c3686d3fda441e75dc3c44eca86b714b876309
Elis Brasil <elis_emb@hotmail.com>
2025-03-14 21:51:44 -0300

    Inclusão da lista através do For

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 let amigos = [];
 let amigo = document.getElementById('amigo');
-//let amigo = document.querySelector('input');
+
 
 function adicionarAmigos() {
 
     if ( amigo == "") {
         alert("Por favor, insira um nome.");
     } else {
-        let listaAmigos = amigos.push(amigo).toString();
+        let str = amigo.value.trim()
+        let listaAmigos = amigos.push(str);
+        montarLista()
         limparCampo()
         console.log(listaAmigos);
+        console.log(amigos)
     } 
 }
 
 function limparCampo() {
     let amigo = document.getElementById('amigo');
     amigo.value = "";
+}
+
+//function exibirLista( tag, texto) {
+//    let lista = document.querySelector(tag);
+//    lista.innerHTML = texto;
+//}
+
+//function montarLista() {
+//        let novoAmigo = amigo.value
+//        console.log(novoAmigo)
+//        let listaAmigos = document.getElementById('listaAmigos')
+//        let li = document.createElement('li')
+//        li.textContent = novoAmigo
+//        listaAmigos.appendChild(li); 
+//}
+        
+
+function montarLista() {
+    let listaAmigos = document.getElementById('listaAmigos')
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');   
+        li.textContent = amigos[i];  
+        listaAmigos.appendChild(li);
+    }  
 }
