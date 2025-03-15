ccd750be3ffe84967329529aae45495bd2ca933b
Elis Brasil <elis_emb@hotmail.com>
2025-03-15 15:39:42 -0300

    inclusão da função sortear Amigo e limpeza do codígo

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 let amigos = [];
-let amigo = document.getElementById('amigo');
+let amigo = document.getElementById("amigo");
 
 function adicionarAmigos() {
-
-    if ( amigo == "") {
+    if (amigo.value.trim() === "") {
         alert("Por favor, insira um nome.");
     } else {
-        let str = amigo.value.trim()
-        let listaAmigos = amigos.push(str);
-        montarLista()
-        limparCampo()
+        let str = amigo.value.trim();
+        amigos.push(str);
+        montarLista();
+        limparCampo();
     } 
 }
 
 function limparCampo() {
-    let amigo = document.getElementById('amigo');
+    let amigo = document.getElementById("amigo");
     amigo.value = "";
 }
 
 function montarLista() {
-    let listaAmigos = document.getElementById('listaAmigos')
+    let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
     for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement('li');   
+        let li = document.createElement('li');  
         li.textContent = amigos[i];  
         listaAmigos.appendChild(li);
     }  
 }
+
+function sortearAmigo() {
+    let qtdAmigos = amigos.length;
+    if (qtdAmigos === 0) {
+        alert("Por favor, insira um amigo.");
+    } else { 
+        let amigoSorteado =  Math.floor(Math.random() * qtdAmigos);
+        let resultado = document.getElementById("resultado");
+        listaAmigos.innerHTML = "";
+        resultado.innerHTML = amigos[amigoSorteado];
+    }  
+}
+
+
